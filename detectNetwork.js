255cf72010d7437774504857620f1d51cb0a61e8
sbasher314 <sbasher314@gmail.com>
2020-10-22 02:33:09 -0400

    Added China UnionPay & Switch Networks, now favors longest found prefix

--- detectNetwork.js
+++ detectNetwork.js
@@ -8,7 +8,6 @@
 //   2. The number of digits in the number (called the length)
 
 var detectNetwork = function(cardNumber) {
-
   function Network(name, prefixArray, cardLength) {
     this.name = name;
     this.prefixes = prefixArray;
@@ -16,32 +15,48 @@ var detectNetwork = function(cardNumber) {
     return this;
   }
 
+  function rangeList(num1, num2) {
+    var higher = (num1>num2)?num1:num2;
+    var lower  = (num1<num2)?num1:num2;
+    var array  = [];
+    for(var i = lower; i<=higher; i++) {
+      array.push(i);
+    }
+    return array;
+  }
+
   var networks = [
     new Network("Visa", [4], [13, 16, 19]),
-    new Network("Discover", [6011, 644, 645, 646, 647, 648, 649, 65], [16, 19]),
-    new Network("Maestro", [5018, 5020, 5038, 6304], [12, 13, 14, 15, 16, 17, 18, 19]),
-    new Network("MasterCard", [51,52,53,54,55],[16]),
+    new Network("Discover", [6011, 644, rangeList(645, 649), 65].flat(), [16, 19]),
+    new Network("Maestro", [5018, 5020, 5038, 6304], rangeList(12,19)),
+    new Network("MasterCard", rangeList(51,55),[16]),
     new Network("Diner's Club", [38,39], [14]),
     new Network("American Express", [34, 37], [15]),
+    new Network("China UnionPay", [rangeList(622126, 622925), 624, 625, 626, rangeList(6282,6288)].flat(), rangeList(16, 19)),
+    new Network("Switch", [4903, 4905, 4911, 4936, 564182, 633110, 6333, 6759], [16,18,19])
   ];
 
-  var prefix = cardNumber.substr(0,2);
   var cardLength = cardNumber.length;
 
   // Iterate through all networks
+  var chosenNetwork;
   for (var i = 0; i<networks.length; i++) {
     var network = networks[i];
+    var longestPrefix = 0;
     //Iterate through each prefix in the current network object
     for (var p = 0; p<network.prefixes.length; p++) {
       var prefixTest = network.prefixes[p];
       if(cardNumber.startsWith(prefixTest)) { //compare the current ,prefix and see if the cardNumber starts with it
         if (network.length.includes(cardLength)) { //if the card length is a valid length for the current Network..
-          return network.name;
+          if(prefixTest.toString().length > longestPrefix) {
+            chosenNetwork = network;
+            longestPrefix = prefixTest.toString().length;
+          }
         }
       }
     }
-
   }
+  if(chosenNetwork !== undefined) { return chosenNetwork.name; }
   console.log("Card Network not Detected");
   // Note: `cardNumber` will always be a string
   // The Diner's Club network always starts with a 38 or 39 and is 14 digits long
